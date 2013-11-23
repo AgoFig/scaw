c8bb3544ec48a718bccd185cf966a6deac45c288
Cris245 <cristians245@gmail.com>
2013-11-23 18:15:54 -0300

    agregado un dropdown con los comentarios

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
+
+  $('#desplega-comentarios').click(function() {
+    $('.comentarios-desplegados').slideToggle('slow');
+  });
 });
