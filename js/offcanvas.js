d2242e8b61a5a157571179a5bf31a3ee2813163d
AgoFig <agostinafigueredo@gmail.com>
2013-11-25 17:39:24 -0300

    Index pasado a PHP. Corregido tambien.

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function() {/*
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
-
+*/
   $('#desplega-comentarios').click(function() {
     $('.comentarios-desplegados').slideToggle('slow');
   });
